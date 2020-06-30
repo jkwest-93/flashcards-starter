05cb4ea2c257f742af24e80944793ba3a60a4469
Jake West <jacobkwest93@gmail.com>
2020-06-30 16:37:17 -0600

    Remove extra code

--- test/Round-test.js
+++ test/Round-test.js
@@ -16,12 +16,10 @@ describe('Round', function() {
     });
 
   it('should be a function', function() {
-    // const round = new Round();
     expect(Round).to.be.a('function');
   })
 
   it('should be an instance of Deck', function() {
-    // const round = new Round();
     expect(round).to.be.an.instanceof(Round);
   })
 
