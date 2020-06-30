1a31680ae094219023bbb8e706bfd9489a1fe296
Jake West <jacobkwest93@gmail.com>
2020-06-30 16:35:46 -0600

    Fix hoisting issues with Round-test file

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,38 +1,35 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Card = require('../src/Card') //mabye
-const Deck = require('../src/Deck') //maybe
+const Card = require('../src/Card')
+const Deck = require('../src/Deck')
 const Round = require('../src/Round')
 
 describe('Round', function() {
+  let card1, card2, card3, deck, round;
+    beforeEach(function () {
+      card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+      card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+      card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+      deck = new Deck([card1, card2, card3]);
+      round = new Round(deck);
+    });
+
   it('should be a function', function() {
-    const round = new Round();
+    // const round = new Round();
     expect(Round).to.be.a('function');
   })
 
   it('should be an instance of Deck', function() {
-    const round = new Round();
+    // const round = new Round();
     expect(round).to.be.an.instanceof(Round);
   })
 
   it('should have a deck', function() {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = new Deck([card1, card2, card3]);
-
-    const round = new Round(deck);
     expect(round.deck).to.deep.equal([card1, card2, card3])
   })
 
   it('should have a current card that is the first card in the deck at the start of the round', function() {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-    const deck = new Deck([card1, card2, card3]);
-
-    const round = new Round(deck);
-    expect(round.returnCurrentCard()).to.deep.equal([card1])
+    expect(round.returnCurrentCard()).to.equal(card1)
   })
 })
