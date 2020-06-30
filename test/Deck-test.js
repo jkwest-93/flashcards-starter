4156319eb4e6ccadd4f3bd67c15513f98a24ede2
Jake West <jacobkwest93@gmail.com>
2020-06-30 12:09:26 -0600

    Fix error & add Card class

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,13 +1,14 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const Card = require('../src/Card')
 const Deck = require('../src/Deck')
 
 describe('Deck', function() {
 
   it('should be a function', function() {
     const deck = new Deck();
-    expect(deck).to.be.a('function');
+    expect(Deck).to.be.a('function');
   })
 
   it('should be an instance of Deck', function() {
