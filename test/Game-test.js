b9874f02a10c1e9cc0c44e6e665a450c72fac9b1
Jake West <jacobkwest93@gmail.com>
2020-06-30 20:29:01 -0600

    Add Game tests

---
+++ test/Game-test.js
@@ -0,0 +1,31 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card')
+const Deck = require('../src/Deck')
+const Round = require('../src/Round')
+const Game = require('../src/Game')
+
+describe('Game', function() {
+  let card1, card2, card3, deck, round, game;
+    beforeEach(function () {
+      card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+      card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+      card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+      deck = new Deck([card1, card2, card3]);
+      round = new Round(deck);
+      game = new Game(round);
+    });
+
+  it('should be a function', function() {
+    expect(Game).to.be.a('function');
+  })
+
+  it('should be an instance of Game', function() {
+    expect(game).to.be.an.instanceof(Game);
+  })
+
+  it('should have a current round', function() {
+    expect(game.currentRound).to.deep.equal(round)
+  })
+})
