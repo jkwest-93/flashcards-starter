dc8f6b8bea1c9bdff6dca3977dbee43ccf81b8e2
Jake West <jacobkwest93@gmail.com>
2020-06-30 19:59:54 -0600

    Add take turn, calculate percent correct, and end round methods

--- src/Round.js
+++ src/Round.js
@@ -1,10 +1,36 @@
+const Turn = require('../src/Turn')
+
 class Round {
   constructor(deck) {
-    this.deck = deck.cards
+    this.deck = deck.cards,
+    this.turns = 0,
+    this.incorrectGuesses = []
   }
 
   returnCurrentCard() {
     return this.deck[0]
+  }
+
+  takeTurn(guess) {
+    let newTurn = new Turn(guess, this.returnCurrentCard())
+
+    if(newTurn.giveFeedback() === 'correct!') {
+      this.deck.splice(0, 1)
+      this.turns ++;
+    } else {
+      this.incorrectGuesses.push(newTurn.returnCard().id)
+      this.deck.push(this.deck.shift())
+      this.turns ++;
+    }
+    return newTurn.giveFeedback()
+  }
+
+  calculatePercentCorrect() {
+    return ((this.turns - this.incorrectGuesses.length) / this.turns) * 100
+  }
+
+  endRound() {
+    console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`)
   }
 }
 
