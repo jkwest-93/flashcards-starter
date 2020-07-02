36029f4e8adf14d6c08d85c0e0f786a983d73d8e
Jake West <jacobkwest93@gmail.com>
2020-07-02 17:27:03 -0600

    Fix decimal point to 2 places

--- src/Round.js
+++ src/Round.js
@@ -30,7 +30,7 @@ class Round {
   }
 
   endRound() {
-    console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`)
+    console.log(`** Round over! ** You answered ${this.calculatePercentCorrect().toFixed(2)}% of the questions correctly!`)
   }
 }
 
